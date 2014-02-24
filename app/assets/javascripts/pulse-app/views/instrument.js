41e677edf2716a63ab37e51387cdd2a70b8894ae
Renfred Harper <renfredxh@gmail.com>
2014-02-23 21:07:50 -0500

    Create base index for pulse-app

--- app/assets/javascripts/pulse-app/views/instrument.js
+++ app/assets/javascripts/pulse-app/views/instrument.js
@@ -3,18 +3,30 @@ var pulse = pulse || {};
 // The DOM element for a todo item...
 pulse.InstrumentView = Backbone.View.extend({
 
-  className: '.instrument',
+  className: '.instrument-row',
 
   // Cache the template function for a single item.
   template: _.template( $('#instrument-template').html() ),
 
+  events: {
+    'click .instrument': 'play'
+  },
+
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.audio = new buzz.sound(this.model.get('sample'), {
+      formats: [ "ogg", "mp3", "wav" ]
+    });
   },
 
   render: function() {
     this.$el.html( this.template( this.model.toJSON() ) );
     return this;
+  },
+
+  // Play the instrument's sample
+  play: function() {
+    this.audio.play();
   },
 
 });
