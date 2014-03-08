7cee74512edd3f20e527d7e7ab994b9a5a7c5b4b
Renfred Harper <renfredxh@gmail.com>
2014-03-08 03:05:53 -0500

    Compartmentalize main backbone app logic

--- app/assets/javascripts/pulse-app/pulse.js
+++ app/assets/javascripts/pulse-app/pulse.js
@@ -1,24 +1,28 @@
-$(document).ready(function() {
+function loadRhythm() {
+  pulse.noteGroupViews = [];
+  $.getJSON("/rhythms/2.json").done(function(data) {
+    $.each(data.columns, function(idx, column) {
+      // Initialize a new noteGroup. The noteGroup is reset after the view is created
+      // so that the view's reset callback can be fired, preventing asyncronyous issues
+      // with rendering.
+      var noteGroup = new pulse.NoteGroup();
+      pulse.noteGroupViews.push(new pulse.NoteGroupView({collection: noteGroup}));
+      noteGroup.reset(column.notes);
+    });
+  });
+}
+
+(function() {
   var kit;
   $.getJSON("/kits/1.json", function(data){
     kit = data;
   }).done(function() {
     pulse.Kit.reset(kit.instruments);
-
-    pulse.noteGroupViews = [];
-    $.getJSON("/rhythms/2.json").done(function(data) {
-      $.each(data.columns, function(idx, column) {
-        // Initialize a new noteGroup. The noteGroup is reset after the view is created
-        // so that the view's reset callback can be fired, preventing asyncronyous issues
-        // with rendering.
-        var noteGroup = new pulse.NoteGroup();
-        pulse.noteGroupViews.push(new pulse.NoteGroupView({collection: noteGroup}));
-        noteGroup.reset(column.notes);
-      });
+    loadRhythm();
   });
+})();
 
-  });
-
+$(document).ready(function() {
 
   $(function() {
     new pulse.AppView();
