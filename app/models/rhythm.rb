af5e9c91a8f59596ccbf0cf0c8f7cfcc0b548393
Renfred Harper <renfredxh@gmail.com>
2014-03-09 21:06:15 -0400

    Only auto-generate patterns in new rhythms

--- app/models/rhythm.rb
+++ app/models/rhythm.rb
@@ -2,20 +2,22 @@ class Rhythm < ActiveRecord::Base
   has_many :patterns, dependent: :destroy
   has_many :notes, through: :patterns
   after_save {
-    8.times do |row|
-      pattern = Pattern.new(
-        rhythm_id: self.id,
-        row: row
-      )
-      pattern.save()
-      16.times do |col|
-        note = Note.new(
-          pattern_id: pattern.id,
-          position: col,
-          volume: 80,
-          active: false
+    if self.patterns.length == 0
+      8.times do |row|
+        pattern = Pattern.new(
+          rhythm_id: self.id,
+          row: row
         )
-        note.save()
+        pattern.save()
+        16.times do |col|
+          note = Note.new(
+            pattern_id: pattern.id,
+            position: col,
+            volume: 80,
+            active: false
+          )
+          note.save()
+        end
       end
     end
   }
